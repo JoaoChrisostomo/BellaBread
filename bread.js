24502d1b4eb0628bffd86a34ace1d5b5c281034f
Joao Chrisostomo <maturandomassas@gmail.com>
2022-03-05 03:03:27 -0300

    parte de selecionar o tamanho do pao e quantidade de paes finalizada

--- bread.js
+++ bread.js
@@ -1,10 +1,98 @@
 let breadJson = [
-    {id:1, name:'Sourdough Abobora', img:'images/sourdough-abobora.jpg', price:25.00, sizes:['100g', '530g', '860g'], description:'Sourdough de abobora fermentação natural'},
-    {id:2, name:'Croassaint', img:'images/croassaint.jpg', price:26.00, sizes:['320g', '530g', '860g'], description:'Croassaint de fermentação natural'},
-    {id:3, name:'Sourdough Integral', img:'images/sourdough-integral.jpg', price:28.00, sizes:['320g', '530g', '860g'], description:'Sourdough integral de fermentação natural'},
-    {id:4, name:'Brioche', img:'images/brioche.jpg', price:30.00, sizes:['320g', '530g', '860g'], description:'Brioche de fermentação natural'},
-    {id:5, name:'Bagette', img:'images/baguette.jpg', price:29.00, sizes:['320g', '530g', '860g'], description:'Baguette Rustica fermentação natural'},
-    {id:6, name:'Foccacia', img:'images/foccacia.jpg', price:35.00, sizes:['320g', '530g', '860g'], description:'Foccacia de alecrim, Sal do himalaya, tomate cereja e manjericão'},
-    {id:7, name:'Super Babka', img:'images/receita-pao-doce-nutella-babka.jpg', price:32.00, sizes:['320g', '530g', '860g'], description:'Babka de nutella fermentação natural'},
-    {id:8, name:'Sourdough Batata Doce', img:'images/sourdough-batata-doce.jpg', price:35.00, sizes:['320g', '530g', '860g'], description:'Creme de leite, lascas de chocolate e morangos'}
+    {
+        id: 1,
+        name: 'Sourdough Abobora',
+        img: 'images/sourdough-abobora.jpg',
+        price: [24.00, 16.00, 8.00],
+        sizes: [
+            '100g', 
+            '350g',
+            '525g'
+        ],
+        description:'Sourdough de abobora fermentação natural'
+    },
+    {
+        id:2,
+        name:'Croassaint',
+        img:'images/croassaint.jpg',
+        price: [7.00, 12.00, 20.00],
+        sizes: [
+            '100g', 
+            '350g',
+            '525g'
+        ],
+        description:'Croassaint de fermentação natural'
+    },
+    {
+        id:3,
+        name:'Sourdough Integral',
+        img:'images/sourdough-integral.jpg',
+        price: [24.00, 16.00, 8.00],
+        sizes: [
+            '100g', 
+            '350g',
+            '525g'
+        ],
+        description:'Sourdough integral de fermentação natural'
+    },
+    {
+        id:4,
+        name:'Brioche',
+        img:'images/brioche.jpg',
+        price: [24.00, 16.00, 8.00],
+        sizes: [
+            '100g', 
+            '350g',
+            '525g'
+        ],
+        description:'Brioche de fermentação natural'
+    },
+    {
+        id:5,
+        name:'Bagette',
+        img:'images/baguette.jpg',
+        price: [24.00, 16.00, 8.00],
+        sizes: [
+            '100g', 
+            '350g',
+            '525g'
+        ],
+        description:'Baguette Rustica fermentação natural'
+    },
+    {
+        id:6,
+        name:'Foccacia',
+        img:'images/foccacia.jpg',
+        price: [24.00, 16.00, 8.00],
+        sizes: [
+            '100g', 
+            '350g',
+            '525g'
+        ],
+        description:'Foccacia de alecrim, Sal do himalaya, tomate cereja e manjericão'
+    },
+    {
+        id:7,
+        name:'Super Babka',
+        img:'images/receita-pao-doce-nutella-babka.jpg',
+        price: [24.00, 16.00, 8.00],
+        sizes: [
+            '100g', 
+            '350g',
+            '525g'
+        ],
+        description:'Babka de nutella fermentação natural'
+    },
+    {
+        id:8,
+        name:'Sourdough Batata Doce',
+        img:'images/sourdough-batata-doce.jpg',
+        price: [24.00, 16.00, 8.00],
+        sizes: [
+            '100g', 
+            '350g',
+            '525g'
+        ],
+        description:'Creme de leite, lascas de chocolate e morangos'
+    }
 ];
--- js/script.js
+++ js/script.js
@@ -1,56 +1,114 @@
+// criar a variavel modalkey para ser global
+let modalKey = 0
+// variavel para controlar a quantidade inicial de breads na modal
+let quantBreads = 1
+// carrinho
+let cart = []
+
 const seleciona = (elemento) => document.querySelector(elemento);
 const selecionaTodos = (elemento) => document.querySelectorAll(elemento);
 
+const formatoReal = (valor) => {
+    return valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+}
+
+const formatoMonetario = (valor) => {
+    if(valor) {
+        return valor.toFixed(2)
+    } 
+}
+
 const abrirModal = () => {
-    seleciona(".breadWindowArea").style.opacity = 0
+    seleciona(".breadWindowArea").style.opacity = 0 // transparent
     seleciona(".breadWindowArea").style.display = "flex"
-    setTimeout(() => {
-        seleciona(".breadWindowArea").style.opacity = 1
-    }, 150)
+    setTimeout(() => seleciona(".breadWindowArea").style.opacity = 1, 150)
 }
 
 const fecharModal = () => {
-    seleciona(".breadWindowArea").style.opacity = 0
-    setTimeout(() => {
-        seleciona(".breadWindowArea").style.display = "none"
-    }, 500)
+    seleciona(".breadWindowArea").style.opacity = 0 // transparent
+    setTimeout(() => seleciona(".breadWindowArea").style.display = "none", 500)
 }
 
 const botoesFechar = () => {
-     // document.querySelector(".breadInfo--cancelButton").addEventListener('click', (e) => {
-    //     document.querySelector(".breadWindowArea").style.display = "none"
-    // })
-
-    // document.querySelector(".breadInfo--cancelMobileButton").addEventListener("click", (e) => {
-    //     document.querySelector(".breadWindowArea").style.display = "none"
-    // })
-    selecionaTodos(".breadInfo--cancelButton, .breadInfo--cancelMobileButton").forEach((item) => {
-        item.addEventListener("click", fecharModal)
-    })
+    selecionaTodos(".breadInfo--cancelButton, .breadInfo--cancelMobileButton").forEach( (item) => item.
+    addEventListener("click", fecharModal) )
 }
 
-const preencheDadosDosPaes = (breadItem, item) => {
+const preencheDadosDosPaes = (breadItem, item, index) => {
+    // setando um atrinuto para cada item do breadItem
+    breadItem.setAttribute("data-key", index)
     breadItem.querySelector('.bread-item--img img').src = item.img
-    breadItem.querySelector('.bread-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    breadItem.querySelector('.bread-item--price').innerHTML = formatoReal(item.price[2])
     breadItem.querySelector('.bread-item--name').innerHTML = item.name
     breadItem.querySelector('.bread-item--desc').innerHTML = item.description
 }
 
 const preencheDadosModal = (item) => {
-     // preenchimento dos dados
-        // document.querySelector(".breadBig img").src = item.img
-        // document.querySelector(".breadInfo h1").innerHTML = item.name
-        // document.querySelector(".breadInfo--desc").innerHTML = item.description
-        // document.querySelector(".breadInfo--actualPrice").innerHTML = `R$ ${item.price.toFixed(2)}`
+     // preenchimento dos dados da janela modal
         seleciona(".breadBig img").src = item.img
         seleciona(".breadInfo h1").innerHTML = item.name
         seleciona(".breadInfo--desc").innerHTML = item.description
-        seleciona(".breadInfo--actualPrice").innerHTML = `R$ ${item.price.toFixed(2)}`    
+        seleciona(".breadInfo--actualPrice").innerHTML = formatoReal(item.price[2])    
 }
 
+const pegarKey = (e) => {
+    // closet retorna o elemento mais proximo que tem a class que passamos
+    // do bread-item ele vai pegar o valor do atributo data-key
 
+    let key = e.target.closest(".bread-item").getAttribute("data-key")
+    console.log('pao clicado' + key)
+    console.log(breadJson[key])
 
+    quantBreads = 1
 
+    modalKey = key
+
+    return key
+}
+
+const preencherTamanhos = (key) => {
+    // tirar a sele de tamanho atual e selecionar o tamanho grande
+    seleciona(".breadInfo--size.selected").classList.remove("selected")
+
+    //selecionar todos os tamanhos
+    selecionaTodos(".breadInfo--size").forEach((size, sizeIndex) => {
+        //selecionar o tamanho grande
+        (sizeIndex == 2) ? size.classList.add("selected") : ''
+        size.querySelector("span").innerHTML = breadJson[key].sizes[sizeIndex]
+    })
+}    
+
+const escolherTamanhoPreco = (key) => {
+    // acoes nos botoes de tamanho
+    // selecionar todos os tamanhos
+    selecionaTodos(".breadInfo--size").forEach((size, sizeIndex) => {
+        size.addEventListener("click", (e) => {
+            // clicou em um item, tirar a selecao dos outros e marca o que voce clicou
+            // tirar a selecao de tamanho atual e selecionar o tamanho grande
+            seleciona(".breadInfo--size.selected").classList.remove("selected")
+            // marcar o que voce clicou, ao inves de usar e.target use size, pois ele é nosso item dentro do loop
+            size.classList.add("selected")
+
+            // mudar o preco de acordo com tamanho do pao
+            seleciona(".breadInfo--actualPrice").innerHTML = formatoReal(breadJson[key].price[sizeIndex])
+        })
+    })
+}
+
+const mudarQuantidade = () => {
+    // Ações nos botões + e - da janela modal
+    seleciona('.breadInfo--qtmais').addEventListener('click', () => {
+        quantBreads++
+        seleciona('.breadInfo--qt').innerHTML = quantBreads
+    })
+
+    seleciona('.breadInfo--qtmenos').addEventListener('click', () => {
+        if(quantBreads > 1) {
+            quantBreads--
+            seleciona('.breadInfo--qt').innerHTML = quantBreads
+        }
+    })
+}
 
 breadJson.map((item, index ) => {
     //console.log(item)
@@ -60,21 +118,35 @@ breadJson.map((item, index ) => {
     seleciona('.bread-area').append(breadItem)
 
     // preencher os dados de cada paes
-    preencheDadosDosPaes(breadItem, item)
+    preencheDadosDosPaes(breadItem, item, index)
 
     // pão for clicado 
     breadItem.querySelector(".bread-item a").addEventListener('click', (e) => {
         e.preventDefault()
         console.log('clicou no pão')
 
-        // abrir a janela modal
-        document.querySelector(".breadWindowArea").style.display = "flex"
-
+        let chave = pegarKey(e)
+        
+        // abrir janela modal
         abrirModal()
 
+        // preenchimento dos dados
         preencheDadosModal(item)
+
+         // pegar tamanho selecionado
+         preencherTamanhos(chave)
+
+        // definir quantidade inicial como 1
+		seleciona('.breadInfo--qt').innerHTML = quantBreads
+
+
+        // selecionar o tamanho e preco com o clique no botao
+        escolherTamanhoPreco(chave)
     })
 
     botoesFechar()
 
 })
+
+// mudar quantidade com os botoes + e -
+mudarQuantidade()
